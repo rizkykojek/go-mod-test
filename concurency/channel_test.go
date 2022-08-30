2f2650ec1e8e763f28dc00f947c5234c4201ba89
Rizky Maulana <rizki.maulana@happyfresh.com>
2022-08-30 15:13:59 +0700

    Add channel parameters as in or out

--- concurency/channel_test.go
+++ concurency/channel_test.go
@@ -37,3 +37,23 @@ func TestChannelAsParameter(t *testing.T) {
 	fmt.Println(data)
 	time.Sleep(5 * time.Second)
 }
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Rizky Kojek"
+}
+
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+}
