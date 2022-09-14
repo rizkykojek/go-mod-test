c14f0e99c084642d7e3db1f92fde7bfd38ef537f
Rizky Maulana <rizki.maulana@happyfresh.com>
2022-09-14 14:38:09 +0700

    Read file byte using embed

--- embed/embed_test.go
+++ embed/embed_test.go
@@ -3,6 +3,8 @@ package embed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"os"
 	"testing"
 )
 
@@ -12,7 +14,17 @@ var version string
 //go:embed VERSION.txt
 var version2 string
 
-func TestEmbedString(t *testing.T) {
+func TestString(t *testing.T) {
 	fmt.Println(version)
 	fmt.Println(version2)
+}
+
+//go:embed logo.png
+var logo []byte
+
+func TestByte(t *testing.T) {
+	err := os.WriteFile("logo_next.png", logo, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
